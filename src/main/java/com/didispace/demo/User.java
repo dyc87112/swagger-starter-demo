60627620b320235f7c55856724f1b515fbcc79ae
didi <dyc87112@qq.com>
2017-08-24 11:21:23 +0800

    样例提交

--- src/main/java/com/didispace/demo/User.java
+++ src/main/java/com/didispace/demo/User.java
@@ -1,5 +1,8 @@
 package com.didispace.demo;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +12,13 @@ import javax.validation.constraints.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("用户基本信息")
 public class User {
 
+    @ApiModelProperty("姓名")
     @Size(max = 20)
     private String name;
+    @ApiModelProperty("年龄")
     @Max(150)
     @Min(1)
     private Integer age;
