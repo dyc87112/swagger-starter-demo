240f0ed7ef49e37abc4b68e487b3cba9d41431c6
didi <dyc87112@qq.com>
2017-08-27 00:00:22 +0800

    样例提交

--- src/main/java/com/didispace/demo/User.java
+++ src/main/java/com/didispace/demo/User.java
@@ -1,6 +1,5 @@
 package com.didispace.demo;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
