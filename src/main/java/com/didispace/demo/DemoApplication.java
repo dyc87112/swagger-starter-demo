d5d22bb3048f6808f991c10faaf27ea8e7cd77a1
翟永超 <dyc87112@qq.com>
2020-12-16 12:36:04 +0800

    2.0.0 测试

--- src/main/java/com/didispace/demo/DemoApplication.java
+++ src/main/java/com/didispace/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.didispace.demo;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableSwagger2Doc
 @SpringBootApplication
 public class DemoApplication {
 
